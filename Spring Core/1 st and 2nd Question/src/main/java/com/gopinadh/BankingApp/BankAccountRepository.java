2edcdd4207bac6b5568725887d6006ba85e27719
Gopinadh Bhupathi <47374417+gopinadh-bhupathi@users.noreply.github.com>
2021-02-14 23:49:31 +0530

    Delete Spring Core/1 st and 2nd Question/src/main/java directory

--- Spring Core/1 st and 2nd Question/src/main/java/com/gopinadh/BankingApp/BankAccountRepository.java
+++
@@ -1,6 +0,0 @@
-package com.gopinadh.BankingApp;
-
-public interface BankAccountRepository {
-	public double getBalance(long accountId);
-	public double updateBalance(long accountId, double newBalance);
-}
